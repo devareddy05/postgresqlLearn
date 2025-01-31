960073efe47d7ac638f91a258f643fe2582d7632
devareddy <send2devareddy@gmail.com>
2025-01-31 10:10:55 +0530

    SpringBoot

---
+++ src/main/java/com/example/postgresqllearn/dto/MeetingDto.java
@@ -0,0 +1,33 @@
+package com.example.postgresqllearn.dto;
+
+import com.example.postgresqllearn.entity.Employee;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MeetingDto {
+
+    private Long meetingId;
+    private String description;
+    private LocalDate date;
+    private LocalTime time;
+    private String organizer;
+
+    private List<Employee> attendees ;
+
+
+
+
+}
